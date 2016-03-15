38aa5782d111df9b4cbefcbef5c17ae072f9df82
pragneshambani <pragnesh.ambani@gmail.com>
2016-03-15 16:45:55 +0530

    Improved Color scheme UI Fixed colors scrolling Showing names of the color

---
+++ app/src/main/java/com/example/sumit/animalplanet/ColorSchemes.java
@@ -0,0 +1,15 @@
+package com.example.sumit.animalplanet;
+
+public class ColorSchemes {
+    int red;
+    int green;
+    int blue;
+    String name;
+
+    public ColorSchemes(int theRed, int theGreen, int theBlue, String colorName){
+        red = theRed;
+        blue = theBlue;
+        green = theGreen;
+        name = colorName;
+    }
+}
--- app/src/main/java/com/example/sumit/animalplanet/ColorWorldActivity.java
+++ app/src/main/java/com/example/sumit/animalplanet/ColorWorldActivity.java
@@ -7,19 +7,41 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class ColorWorldActivity extends AppCompatActivity {
+
+    ArrayList<ColorSchemes> colorSchemes = new ArrayList<>();
+    int colorIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_world);
+        populateColorSchemes();
     }
+
+    private void populateColorSchemes(){
+        colorSchemes.add(new ColorSchemes(0,0,0,"Black"));
+        colorSchemes.add(new ColorSchemes(255,0,0,"Red"));
+        colorSchemes.add(new ColorSchemes(0,255,0,"Green"));
+        colorSchemes.add(new ColorSchemes(0,0,255,"Blue"));
+        colorSchemes.add(new ColorSchemes(255,255,0,"Yellow"));
+        colorSchemes.add(new ColorSchemes(255,165,0,"Orange"));
+        colorSchemes.add(new ColorSchemes(165,42,42,"Brown"));//
+        colorSchemes.add(new ColorSchemes(255,192,203,"Pink"));
+        colorSchemes.add(new ColorSchemes(128,0,128,"Purple"));
+    }
+
+
     public void changeMyColor(View view) {
-        Random rnd = new Random();
-        int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
-        view.setBackgroundColor(color);
+        if(colorIndex == colorSchemes.size()) colorIndex = 0;
+        int color = Color.argb(255, colorSchemes.get(colorIndex).red, colorSchemes.get(colorIndex).green, colorSchemes.get(colorIndex).blue);
+        findViewById(R.id.startView).setBackgroundColor(color);
+        ((TextView)findViewById(R.id.starColorName)).setText(colorSchemes.get(colorIndex).name);
+        colorIndex++;
     }
 }
